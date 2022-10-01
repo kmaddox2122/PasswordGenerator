c4070030acfebbbd86b3b22fad29dab881f071c5
Katie Maddox <kmaddox2122@gmail.com>
2022-09-30 20:27:24 -0500

    added pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,21 @@
 // Don't change any original code!
 
+// pseudocode
+// click button to generate password and be prompted with password criteria
+// When prompted with criteria, have options to select certain criteria
+// Options are:
+  // -length of password (8-128 CHAR)
+  // -character types (lwr case, upr case, numeric, sp char.
+// when prompts are answered,input should be validated - at least one char type selected
+// when all prompts answered, pass is generated that matches criteria
+// when pass is generated, it is displayed in an alert 
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function promptMe(){
+    var generateBtn = prompt("Please choose from the following password criteria:");
 
 // Write password to the #password input
 function writePassword() {
